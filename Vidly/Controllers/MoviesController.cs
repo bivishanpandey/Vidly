a2ee4cacf66564524ec6cf50603121f60be24025
bivishanpandey <natsu.dragneel.bivishan@gmail.com>
2018-11-17 18:31:02 +0545

    Code first db for Customer and Movie with identity

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -17,7 +18,49 @@ namespace Vidly.Controllers
                 Id = 1,
                 Name = "Harry Potter"
             };
-            return View(movie);
+            var customers=new List<Customer>
+            {
+                new Customer(){Name = "c1"},
+                new Customer(){Name = "C2"}
+            };
+            var viewmodel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            //ViewData["Movie"] = movie;
+            //return View();
+            return View(viewmodel);
+            //return Content("Hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleasedDate(int year,int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
---
+++ Vidly/Models/IdentityModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vidly.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("ConnectionString", throwIfV1Schema: false) { }
+
+        public DbSet<Customer> Customers { get; set; }  
+        public DbSet<Movie> Movies { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
